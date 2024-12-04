aa205ffa0524d44151ac22780a6cc5266e9d19ea
JoanaaMartins <147702886+JoanaaMartins@users.noreply.github.com>
2024-12-04 12:20:52 +0000

    navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,35 @@
-<template>
+<template> 
+  <div>
+        <nav class="navbar navbar-expand-lg" style="background-color: #B84EFF; font-family:'Ysabeau Infant' ;">
+        <div class="container-fluid">
+            <a class="navbar-brand" style="margin-left: 2%; margin: 1%; height: 60px;" href="#"><img style="height: 60px;" src="../assets/homepageAssets/logoBranco.png" alt=""></a>
+            
+            <div class="nav justify-content-end"  id="navbarSupportedContent" style="margin-right: 1%;" >
+                <ul class="nav ">     <!-- nav-underline -->
+                <li class="nav-item">
+                    <a class="nav-link active" style="color: white;"  aria-current="page" href="#">About us</a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link active" style="color: white;" aria-current="page" href="#">Calendar</a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link active" style="color: white;"  aria-current="page" href="#">Packs</a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link active" style="color: white;"  aria-current="page" href="#">Workshops</a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link active" style="color: white;"  aria-current="page" href="#">Shows</a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link active" style="color: white;" aria-current="page" href="#">(icon)</a>
+                </li>
+                </ul> 
+            </div>
+            </div>
+        </nav>
+    </div> 
+<!-- mariana -->
   <div>
     <h1>Homepage</h1>
     <router-link to="/register">
