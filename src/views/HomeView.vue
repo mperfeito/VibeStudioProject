d09cb730382cda2003afeb77765eae7c99e365c7
JoanaaMartins <147702886+JoanaaMartins@users.noreply.github.com>
2024-12-20 10:15:12 +0000

    aaa

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,8 @@
-<template> 
-  <div>
-        <nav class="navbar navbar-expand-lg" style="background-color: #B84EFF; font-family:'Ysabeau Infant' ;">
-        <div class="container-fluid">
-            <a class="navbar-brand" style="margin-left: 2%; margin: 1%; height: 60px;" href="#"><img style="height: 60px;" src="../assets/logo/logoBranco.png" alt=""></a>
-            
-            <div class="nav justify-content-end"  id="navbarSupportedContent" style="margin-right: 1%;" >
-                <ul class="nav ">     <!-- nav-underline -->
-                <li class="nav-item">
-                    <a class="nav-link active" style="color: white;"  aria-current="page" href="#">About us</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" style="color: white;" aria-current="page" href="#">Calendar</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" style="color: white;"  aria-current="page" href="#">Packs</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" style="color: white;"  aria-current="page" href="#">Workshops</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" style="color: white;"  aria-current="page" href="#">Shows</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link active" style="color: white;" aria-current="page" href="#">(icon)</a>
-                </li>
-                </ul> 
-            </div>
-            </div>
-        </nav>
-    </div> 
-<!-- mariana -->
-  <div>
+<template>  
+<div>
+    <NavBar @change-content="setContent"></NavBar>
+  </div>  
+  <div> 
     <h1>Homepage</h1>
     <router-link to="/register">
       <a href="register">register</a>
@@ -49,6 +21,7 @@
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
 import { useUsersStore } from "@/stores/users.js";
 export default {
   data() {
@@ -56,7 +29,7 @@ export default {
       store: useUsersStore(),
     };
   },
-  methods: {
+  methods: { 
     logoutUser() {
       this.store.logout();
     },
