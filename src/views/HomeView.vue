c69aedefcd4caa642055ef6cc886c8d580acd49f
mperfeito <mperfeito.08@gmail.com>
2024-12-23 19:16:06 +0000

    resolve conflit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
-<template> 
-  
+<template>
   <div>
     <h1>Homepage</h1>
     <router-link to="/register">
@@ -8,30 +7,28 @@
     <router-link to="/login">
       <a href="login">login</a>
     </router-link>
-    
-    
+    <router-link to="/calendar">
+      <a href="login">calendar</a>
+    </router-link>
+    <router-link to="/packs">
+      <a href="login">packs</a>
+    </router-link>
     <button @click="logoutUser()" class="btn btn-dark">Logout</button>
   </div>
 </template>
 
 <script>
-
 import { useUsersStore } from "@/stores/users.js";
-export default { 
-
+export default {
   data() {
     return {
       store: useUsersStore(),
     };
   },
-  methods: { 
+  methods: {
     logoutUser() {
       this.store.logout();
-<<<<<<< HEAD
-      localStorage.clear();
-=======
       local
->>>>>>> 2c6a67e (changes)
     },
   },
 };
